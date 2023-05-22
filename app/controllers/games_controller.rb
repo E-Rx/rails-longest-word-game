106c0a66ad9766fdc02c655e56291f790a4c7bbf
elise rochaix <elise.rochaix@gmail.com>
2023-05-22 23:14:25 +0200

    lemotlepluslong

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -4,29 +4,23 @@ class GamesController < ApplicationController
 
   ALPHABET = ('A'..'Z').to_a
 
+
   def score
-
-    @answer = params[:answer]
-    @picked_letters = params[:letters]
-
-    response = URI.open("https://wagon-dictionary.herokuapp.com/#{@answer}")
-    api_data = JSON.parse(response.read)
-
-    def available_word
-      @answer.chars.sort.all? { |letter| @answer.include?(@letters) }
-    end
-
-    if available_word = true
-      if api_data['found']
-        @case = "Congratulations! #{@answer} is a valid English word!"
-      else
-        @case = "Sorry but #{@answer.uppercase} does not seems to be a valid English word"
-      end
-    else
-      @case = "Sorry but #{@answer.uppercase} can't be built out of #{@picked_letters}"
-    end
-
+    @letters = params[:letters].split
+    @word = (params[:word] || '').upcase
+    @included = included?(@word, @letters)
+    @english_word = english_word?(@word)
   end
+
+  def included?(word, letters)
+    word.chars.all? { |letter| word.count(letter) <= letters.count(letter) }
+  end
+
+  def english_word?(word)
+    response = URI.open("https://wagon-dictionary.herokuapp.com/#{word}")
+    json = JSON.parse(response.read)
+    json['found']
+    end
 
   def new
     @letters = 10.times.map { ALPHABET.sample }
